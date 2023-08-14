0037b2f651bf8cb1b18d6d62eeb0a7242fe77b8a
Lucas-E <lucasgitacc@gmail.com>
2023-08-14 16:05:15 -0300

    added data to request

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -2,16 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 import {cookies} from 'next/headers'
 
 export async function POST(request:NextRequest, response:NextResponse){
+    const data = await request.json()
     const responseAuth = await fetch('https://api.escuelajs.co/api/v1/auth/login', {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
           },
         body: JSON.stringify({
-            email: "john@mail.com",
-            password: "changeme"
+            email: data.email,
+            password: data.password
         })
     })
+    if(!responseAuth.ok){
+        return NextResponse.json({message: 'error'}, {status: 400})
+    }
     const awaitedResponse = await responseAuth.json()
     const cookieStore = cookies();
     cookieStore.set('accessToken', awaitedResponse.access_token, {httpOnly:true})
