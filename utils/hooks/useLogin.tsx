d99ed2a6a14e9fedeea9279a04462ac4bc4d347c
Lucas-E <lucasgitacc@gmail.com>
2023-08-14 16:05:29 -0300

    added safety mesurements

--- utils/hooks/useLogin.tsx
+++ utils/hooks/useLogin.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 
 export const useLogin = () => {
@@ -7,8 +7,7 @@ export const useLogin = () => {
 
 	const [password, setPassword] = useState<string>("");
 
-    const router = useRouter()
-	
+	const router = useRouter();
 
 	const handleChangeEmail = (e: React.FormEvent<HTMLInputElement>) => {
 		setEmail(e.currentTarget.value);
@@ -16,19 +15,23 @@ export const useLogin = () => {
 	const handleChangePassword = (e: React.FormEvent<HTMLInputElement>) => {
 		setPassword(e.currentTarget.value);
 	};
-    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        const response = await fetch('http://localhost:3000/api/auth', {
-            method: 'POST',
-            body: JSON.stringify({
-                email,
-                password
-            })
-        })
-        const awaitedResponse = await response.json()
-        const cookies = Cookies.get()
-        router.push('/teste')
-    }
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const response = await fetch("http://localhost:3000/api/auth", {
+			method: "POST",
+			body: JSON.stringify({
+				email: email,
+				password: password,
+			}),
+		});
+		if (!response.ok) {
+			console.log("error");
+		} else {
+			const awaitedResponse = await response.json();
+			const cookies = Cookies.get();
+			router.push("/teste");
+		}
+	};
 
 	const changeAuthState = {
 		handleChangeEmail,
@@ -36,9 +39,10 @@ export const useLogin = () => {
 	};
 
 	return {
-		email, password,
+		email,
+		password,
 		handleChangeEmail,
-        handleChangePassword,
-        handleSubmit
+		handleChangePassword,
+		handleSubmit,
 	};
 };
