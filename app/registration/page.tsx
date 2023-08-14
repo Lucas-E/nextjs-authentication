60d569bc2d85003b14c2eeae050e0669d2853eff
Lucas-E <lucasgitacc@gmail.com>
2023-08-14 14:59:23 -0300

    added registration page

---
+++ app/registration/page.tsx
@@ -0,0 +1,8 @@
+import RegistrationForm from "@/components/RegistrationForm/RegistrationForm";
+const page = () => {
+	return (
+		<RegistrationForm></RegistrationForm>
+	);
+};
+
+export default page;
